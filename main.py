c617d9936198dd9d9bc082bdb6f0c9c0a2c55a1d
root <root@zstest-1.us-central1-a.c.imperial-palace-250313.internal>
2019-09-19 08:47:45 +0000

    cc

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/test')
+def test():
+    return 'zstest!'
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0')
+
