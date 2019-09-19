7cf4cda87d375165e78c4d7a52c54c7a96221069
root <root@zstest-1.us-central1-a.c.imperial-palace-250313.internal>
2019-09-19 08:49:45 +0000

    ss

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/test')
-def test():
-    return 'zstest!'
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
 
